613f1d5a70e1c0afbc25b602013f6f00d25d5da4
carriegale2710 <carriegale2710@gmail.com>
2025-07-08 15:26:23 +1000

    backend: creates basic CRUD operations for API and outlines e2e testcases

--- employee/src/main/java/io/carrie/employee/employee/EmployeeController.java
+++ employee/src/main/java/io/carrie/employee/employee/EmployeeController.java
@@ -1,12 +1,19 @@
 package io.carrie.employee.employee;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/employees")
@@ -23,5 +30,31 @@ public class EmployeeController {
         List<Employee> allEmployees = this.employeeService.findAll();
         return new ResponseEntity<>(allEmployees, HttpStatus.OK);
     }
+
+    @GetMapping("{/id}")
+    public ResponseEntity<Employee> getById(@PathVariable Integer id) throws Exception {
+        Optional<Employee> foundEmployee = this.employeeService.findById(id);
+        if (foundEmployee.isPresent()) {
+            return new ResponseEntity<>(foundEmployee.get(), HttpStatus.OK);
+        }
+        throw new Exception("Employee " + id + " not found.");
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> create(@Valid @RequestBody CreateEmployeeDTO data) {
+        Employee newEmployee = this.employeeService.create(data);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteById(@PathVariable Integer id) throws Exception {
+        boolean deleted = this.employeeService.deleteById(id);
+        if (deleted) {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
+        throw new Exception("Employee " + id + " not found.");
+    }
+
+    // @PutMapping
 
 }
