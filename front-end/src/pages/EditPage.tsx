1a8b07551bd79aef6f62a6b494a2962a2fa69a56
carriegale2710 <carriegale2710@gmail.com>
2025-07-17 08:14:22 +1000

    ui: creates pages for list and form

---
+++ front-end/src/pages/EditPage.tsx
@@ -0,0 +1,16 @@
+import Form from "../components/Form/Form";
+import { employee1 } from "../services/mockEmployees";
+
+//needs to preload data from selected employee from Card
+// you come here on Edit Button
+
+const EditPage = () => {
+  return (
+    <>
+      <h1>Edit Employee</h1>
+      <Form prefilled={employee1} />
+    </>
+  );
+};
+
+export default EditPage;
