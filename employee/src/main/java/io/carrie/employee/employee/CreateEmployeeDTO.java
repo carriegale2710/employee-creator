305f03a18e2b595812ca53f77aa58185e60d6d17
carriegale2710 <carriegale2710@gmail.com>
2025-07-20 01:10:21 +1000

    tests: handle errors for invalid name inputs

--- employee/src/main/java/io/carrie/employee/employee/CreateEmployeeDTO.java
+++ employee/src/main/java/io/carrie/employee/employee/CreateEmployeeDTO.java
@@ -2,16 +2,26 @@ package io.carrie.employee.employee;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class CreateEmployeeDTO {
 
     @NotBlank
+    @NotNull
+    // @Size(min = 2, max = 30)
+    @Pattern(regexp = "^[a-zA-Z]{2,30}$", message = "First name must be 2 to 30 characters long and contain only letters.")
     private String firstName;
 
     @NotBlank
+    @NotNull
+    // @Size(min = 2, max = 30)
+    @Pattern(regexp = "^[a-zA-Z]{2,30}$", message = "Last name must be 2 to 30 characters long and contain only letters.")
     private String lastName;
 
     @Email
+    @NotNull
     private String email;
 
     public CreateEmployeeDTO() {
