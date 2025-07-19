44a82bc3a0cc5dfea5c902bb26c7016bc6445550
carriegale2710 <carriegale2710@gmail.com>
2025-07-20 02:05:28 +1000

    creates global exception handler

---
+++ employee/src/main/java/io/carrie/employee/common/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package io.carrie.employee.common.exceptions;
+
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException ex) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBadRequest(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+}
