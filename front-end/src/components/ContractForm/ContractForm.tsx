1fc2f37a0e81ebba9d52051d659ae1c08164480f
carriegale2710 <carriegale2710@gmail.com>
2025-07-23 09:08:19 +1000

    feat: creates Duplicate Contract Form for preloading previous contracts as template

--- front-end/src/components/ContractForm/ContractForm.tsx
+++ front-end/src/components/ContractForm/ContractForm.tsx
@@ -1,6 +1,6 @@
 import { useFormStatus } from "react-dom";
 import Button from "../Button/Button";
-import Input from "./ContractInput";
+import Input from "../Input/Input";
 import type { Contract } from "../../services/contracts";
 
 export interface ContractFormProps {
@@ -15,35 +15,48 @@ const ContractForm = ({ prefilled }: ContractFormProps) => {
   // }
 
   return (
-    <form
-    // action={onSubmit}
-    >
-      <h2>Employee Details</h2>
-      <Input label="employeeId" type="number" value={prefilled?.employee.id}>
-        Employee ID
-      </Input>
-      <Input label="department" type="select" value={prefilled?.department}>
-        Department
-      </Input>
-      <Input label="contractType" type="select" value={prefilled?.contractType}>
-        Contract Type
-      </Input>
-      <Input label="salaryAmount" type="number" value={prefilled?.salaryAmount}>
-        Salary Amount
-      </Input>
-      <Input label="hoursPerWeek" type="number" value={prefilled?.hoursPerWeek}>
-        Hours per Week
-      </Input>
-      <Input label="startDate" type="date" value={prefilled?.startDate}>
-        Start Date
-      </Input>
-      <Input label="endDate" type="date" value={prefilled?.endDate}>
-        End Date
-      </Input>
-      <Button type="submit" disabled={pending}>
-        {pending ? "Submitting..." : "Submit"}
-      </Button>
-    </form>
+    <>
+      <form
+      // action={onSubmit}
+      >
+        <Input label="employeeId" type="number" value={prefilled?.employee.id}>
+          Employee ID
+        </Input>
+        <Input label="department" type="select" value={prefilled?.department}>
+          Department
+        </Input>
+        <Input
+          label="contractType"
+          type="select"
+          value={prefilled?.contractType}
+        >
+          Contract Type
+        </Input>
+        <Input
+          label="salaryAmount"
+          type="number"
+          value={prefilled?.salaryAmount}
+        >
+          Salary Amount
+        </Input>
+        <Input
+          label="hoursPerWeek"
+          type="number"
+          value={prefilled?.hoursPerWeek}
+        >
+          Hours per Week
+        </Input>
+        <Input label="startDate" type="date" value={prefilled?.startDate}>
+          Start Date
+        </Input>
+        <Input label="endDate" type="date" value={prefilled?.endDate}>
+          End Date
+        </Input>
+        <Button type="submit" disabled={pending}>
+          {pending ? "Submitting..." : "Submit"}
+        </Button>
+      </form>
+    </>
   );
 };
 
